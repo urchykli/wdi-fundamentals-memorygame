1ef6fb2ed94a7e1b85a6474a979f9ae655ebeac2
Lindsay Urchyk <lindsayurchyk@Lindsays-MacBook-Pro.local>
2018-12-03 16:01:55 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,21 +3,21 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[1];
-
-var cardTwo = cards[0]
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo)
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!")
+		alert("You found a match!");
+		} else {
+			alert("Sorry, try again");
+		}
 	}
-	else {
-		alert("Sorry, try again")
-	}
+
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	if (cardsInPlay.length === 2);
+	checkForMatch();
 }
+
+flipCard(0);
+flipCard(2);
+
